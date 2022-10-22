ae3cf88c1947811e8ddfbb81429886026991a355
annakurylo <anna.k.2003.g@gmail.com>
2022-10-22 23:26:12 +0300

    add sonar eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,11 +3,12 @@ module.exports = {
     node: true,
     es2021: true
   },
-  extends: ['standard', 'prettier'],
+  extends: ['standard', 'prettier', 'plugin:sonarjs/recommended'],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
+  plugins: 'sonarjs',
   rules: {}
 }
